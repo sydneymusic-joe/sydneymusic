2628e1284f2044b95dd0c55128928dc420fa6fa4
Joe Hardy <joe@hardy.id.au>
2022-06-06 17:37:37 +1000

    A quick start at a Contact page

--- src/routes/contact.svelte
+++ src/routes/contact.svelte
@@ -1,1 +1,13 @@
-contact page
+<div class="max-w-5xl px-5 mx-auto space-y-32 pb-24">
+	<!-- First section -->
+	<div class="space-y-10">
+		<img src="https://picsum.photos/1000/150?grayscale" alt="" class="aspect-banner w-full" />
+
+		<div class="space-y-10 pr-20 lg:pr-28">
+			<h2 class="notch-left text-xl">Contact Us</h2>
+            
+            <p>Contacting us is pretty simple! You can <a href="mailto:contact@sydneymusic.net">e-mail us</a> any feedback, questions, gig submissions, ideas or thoughts.</p>
+
+        </div>
+    </div>
+</div>
