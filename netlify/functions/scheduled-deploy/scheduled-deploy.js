34b877fbec0d52236a4996135a65088d4610ff99
Joe Hardy <joe@hardy.id.au>
2022-06-13 17:06:18 +1000

    Added scheduled function for rebuild

---
+++ netlify/functions/scheduled-deploy/scheduled-deploy.js
@@ -0,0 +1,15 @@
+const { schedule } = require('@netlify/functions')
+const REBUILD_URL = process.env.BUILD_TRIGGER_URL;
+
+// To learn about scheduled functions and supported cron extensions,
+// see: https://ntl.fyi/sched-func
+module.exports.handler = schedule('10 0 * * *', async (event) => {
+  const eventBody = JSON.parse(event.body)
+  console.log(`Next function run at ${eventBody.next_run}.`)
+
+  await fetch(REBUILD_URL, { method: 'POST'});
+
+  return {
+    statusCode: 200,
+  }
+})
