7d79182e310cdc28270b3e3ebae0a1969b22ccde
Joe Hardy <joe@hardy.id.au>
2022-06-13 17:15:58 +1000

    Fix netlify function crash

--- netlify/functions/scheduled-deploy/scheduled-deploy.js
+++ netlify/functions/scheduled-deploy/scheduled-deploy.js
@@ -4,8 +4,10 @@ const REBUILD_URL = process.env.BUILD_TRIGGER_URL;
 // To learn about scheduled functions and supported cron extensions,
 // see: https://ntl.fyi/sched-func
 module.exports.handler = schedule('10 0 * * *', async (event) => {
-  const eventBody = JSON.parse(event.body)
-  console.log(`Next function run at ${eventBody.next_run}.`)
+  if (event.body) {
+    const eventBody = JSON.parse(event.body)
+    console.log(`Next function run at ${eventBody.next_run}.`)
+  }
 
   await fetch(REBUILD_URL, { method: 'POST'});
 
