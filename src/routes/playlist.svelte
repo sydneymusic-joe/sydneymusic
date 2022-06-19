ed012b654bf49e79dafca062e65c3b04152f4836
Joe Hardy <joe@hardy.id.au>
2022-06-19 18:37:29 +1000

    playlist og description

--- src/routes/playlist.svelte
+++ src/routes/playlist.svelte
@@ -3,7 +3,7 @@
 	import PlaylistPromo from '$lib/components/playlist.svelte';
 </script>
 
-<SeoSocial title="Gig Guide Playlist" />
+<SeoSocial title="Gig Guide Playlist" description="The SydneyMusic.net playlist is updated every Sunday and is streaming on Apple Music, Spotify and Tidal." />
 
 <div class="max-w-5xl px-5 mx-auto space-y-32 pb-24">
 	<div class="space-y-10">
