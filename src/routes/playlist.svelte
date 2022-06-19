1c963e129bf59e088faa2ed6d460f52254eb2411
Joe Hardy <joe@hardy.id.au>
2022-06-19 18:34:46 +1000

    Title for Playlist page

--- src/routes/playlist.svelte
+++ src/routes/playlist.svelte
@@ -1,6 +1,9 @@
 <script context="module">
+	import SeoSocial from '$lib/components/seo-social.svelte';
 	import PlaylistPromo from '$lib/components/playlist.svelte';
 </script>
+
+<SeoSocial title="Gig Guide Playlist" />
 
 <div class="max-w-5xl px-5 mx-auto space-y-32 pb-24">
 	<div class="space-y-10">
