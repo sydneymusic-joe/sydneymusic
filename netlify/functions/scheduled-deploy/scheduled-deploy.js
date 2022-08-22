1115be6c53340a5193d95fdcf4c50a2d22719799
Joe Hardy <joe@hardy.id.au>
2022-08-23 00:00:42 +1000

    schedule fix

--- netlify/functions/scheduled-deploy/scheduled-deploy.js
+++ netlify/functions/scheduled-deploy/scheduled-deploy.js
@@ -5,7 +5,7 @@ const REBUILD_URL = process.env.BUILD_TRIGGER_URL;
 
 // To learn about scheduled functions and supported cron extensions,
 // see: https://ntl.fyi/sched-func
-module.exports.handler = schedule('10 0 * * *', async (event) => {
+module.exports.handler = schedule('10 10-11 * * *', async (event) => {
   if (event.body) {
     const eventBody = JSON.parse(event.body)
     console.log(`Next function run at ${eventBody.next_run}.`)
