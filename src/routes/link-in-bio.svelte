aaba210e36a543a5e715945e9217ccbc268cb013
Joe Hardy <joe@hardy.id.au>
2023-01-05 15:57:19 +1100

    Change to link-in-bio

--- src/routes/link-in-bio.svelte
+++ src/routes/link-in-bio.svelte
@@ -2,15 +2,14 @@
 <div class="max-w-5xl px-5 mx-auto space-y-32 pb-24">
 	<div class="space-y-10">
 		<div class="space-y-10 sm:pr-20 lg:pr-28">
-			<h3 class="notch-left text-lg lg:text-xl">Link in Bio</h3>
+			<h3 class="notch-left text-lg lg:text-xl">No Ads. No Algorithms. No Shit.</h3>
             
 			<div class="prose">
-				<p>Hi! We made this site so you don't need to rely on Instagram, Facebook or other multi-billion dollar platforms to find out about what's happening in your local community.</p>
+				<p>Hello. We made this site so you don't need to rely on Instagram, Facebook or other multi-billion dollar platforms to find out about what's happening in your local community.</p>
 
 				<p>Here are some links to try:</p>
 
 				<ul>
-					<li><strong>We're throwing an end-of-year party on December 20!</strong> <a href="https://www.eventbrite.com/e/sydneymusicnet-x-crosstalk-records-merch-launch-and-xmas-party-tickets-481515424597">RSVP here</a>.</li>
 					<li>Our <a href="/gig-guide">gig guide</a> has every single gig in Sydney, on one page, with no ads, algorithms or bullshit.</li>
 					<li>Our <a href="https://store.sydneymusic.net">online store</a> is where you can buy merch or donate to help us keep this running.</li>
 					<li>We have a <a href="https://sydneymusic.us17.list-manage.com/subscribe?u=33fe15202bc9075111c10636a&id=08cf9e0f0b">weekly e-mail newsletter</a> where we send out the week's upcoming gigs, every week, for free in your inbox.</li>
