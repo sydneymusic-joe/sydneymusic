5d004fac2a6fbaf498c0768a89f3edff65cee9a3
Joe Hardy <joe@hardy.id.au>
2023-01-06 17:32:17 +1100

    More dumb stuff

--- src/lib/components/feedprompt.svelte
+++ src/lib/components/feedprompt.svelte
@@ -1,6 +1,8 @@
 <script>
     let prompts = [
         'Want to submit a gig? <a href="mailto:contact@sydneymusic.net">E-mail us</a>!',
+        'Smile! Can Man loves you',
+        'Want to write a blurb for a gig? E-mail us!',
         'Your <a href="https://store.sydneymusic.net">donations</a> help keep us running!',
         'Our <a href="https://store.sydneymusic.net">online store</a> is now open'
     ]
