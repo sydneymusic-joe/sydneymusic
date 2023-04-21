55946c6409ac3b8d1069ab1a3573645fc7386a48
Joe Hardy <joe@hardy.id.au>
2023-04-21 15:06:44 +1000

    Merge prompts

--- src/lib/components/feedprompt.svelte
+++ src/lib/components/feedprompt.svelte
@@ -1,8 +1,8 @@
 <script>
     let prompts = [
-        'Want to submit a gig? <a href=“mailto:contact@sydneymusic.net”>E-mail us</a>!',
-        'Are we missing your favourite venue? <a href=“mailto:contact@sydneymusic.net”>Let us know</a>!',
-        'You don\'t have to be in a band to send us gigs we’ve missed! <a href=“mailto:contact@sydneymusic.net”>We love a hot tip.</a>',
+        'Want to submit a gig? <a href="mailto:contact@sydneymusic.net">E-mail us</a>!',
+        'Are we missing your favourite venue? <a href="mailto:contact@sydneymusic.net">Let us know</a>!',
+        'You don\'t have to be in a band to send us gigs we’ve missed! <a href="mailto:contact@sydneymusic.net">We love a hot tip.</a>',
         'The <a href="/playlist">SydneyMusic playlist</a> (available on Spotify, Apple Music and TIDAL) is updated each Wednesday. Hit play, discover something new and then go see it live.',
         'Our <a href="https://store.sydneymusic.net">online store</a> is now open! If you find this site useful, your support is greatly appreciated: every donation or merch sale helps us cover our operating costs.',
         'SydneyMusic is for everyone. <a href="mailto:contact@sydneymusic.net">Send us feedback</a> any time.',
