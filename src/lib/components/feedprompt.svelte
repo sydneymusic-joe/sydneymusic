758b08a8ea6f59401976e01ed614a4204163551b
Joe Hardy <joe@hardy.id.au>
2023-04-21 15:22:01 +1000

    Pedants gonna pedant

--- src/lib/components/feedprompt.svelte
+++ src/lib/components/feedprompt.svelte
@@ -2,7 +2,7 @@
     export let Index = 0;
     let prompts = [
         'The <a href="/playlist">SydneyMusic playlist</a> (available on Spotify, Apple Music and TIDAL) is updated each Wednesday. Hit play, discover something new and then go see it live.',
-        'Want to submit a gig? <a href="mailto:contact@sydneymusic.net">E-mail us</a>!',
+        'Want to submit a gig? <a href="mailto:contact@sydneymusic.net">Email us</a>!',
         'The SydneyMusic Discord is a friendly online community of live music loving individuals discussing our local music community. Everyone is welcome: <a href="https://discord.gg/HuzdkUct">join in here</a>!',
         'You don\'t have to be in a band to send us gigs we’ve missed! <a href="mailto:contact@sydneymusic.net">We love a hot tip.</a>',
         'Our <a href="https://store.sydneymusic.net">online store</a> is now open! If you find this site useful, your support is greatly appreciated: every donation or merch sale helps us cover our operating costs.',
