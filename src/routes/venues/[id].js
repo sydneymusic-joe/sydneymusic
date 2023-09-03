5a436a8e4eb63cddb5fe093a6e029bb016da9725
Joe Hardy <joe@hardy.id.au>
2023-09-03 22:41:34 +1000

    Fixed data retrieval bug

--- src/routes/venues/[id].js
+++ src/routes/venues/[id].js
@@ -1,76 +1,56 @@
 import API from "$lib/contentful/" 
 import { previewMode, formatDateLong } from '$lib/globals.mjs';
 
-
 export async function get({ params }) {
-    data = await API(`query {
-		venuesCollection(where: { slug: "${params.id}" }, limit:1, preview : ${previewMode} ) {
-		  items {
-			venueName,
-			address,
-			suburb,
-			postcode,
-			url,
-			isRip,
-			sys {
-				id
-			}
-		  }
-		}
-	}`)
+	let data=false;
+	let gigs = null
 
-	venueInfo = data.venuesCollection.items[0];
-	const d = new Date();
+	try {
+		const d = new Date();
 
-	/*gigs = await API(`query {
-		eventsCollection(
-			limit : 50,
-			order : [gigStartDate_ASC],
-			where : {gigStartDate_gte : "${new Date(d.setHours(0)).toISOString()}"},
-			preview : ${previewMode}
+		data = await API(`query {
+			venuesCollection(
+				limit : 1,
+				where : { slug : "${params.id}" }
 			) {
 				items {
-					gigStartDate,
-					promotedName,
-					performersList,
-					venue(where: {slug : "the-oxford-tavern"}) {
-						slug
+					venueName,
+					address,
+					suburb,
+					postcode,
+					url,
+					isRip,
+					sys {
+						id
 					},
-					ticketUrl,
-					furtherInfo,
-					furtherInfoContributorInitials,
-					isFree
-				}
-			}
-		}`)
-		*/
-	gigs = await API(`query {
-		venuesCollection(
-			limit : 1,
-			where : { slug : "${params.id}" }
-		) {
-			items {
-				linkedFrom {
-					eventsCollection {
-						total
-						items {
-							gigStartDate,
-							promotedName,
-							performersList,
-							ticketUrl,
-							furtherInfo,
-							furtherInfoContributorInitials,
-							isFree
+					linkedFrom {
+						eventsCollection {
+							total
+							items {
+								gigStartDate,
+								promotedName,
+								performersList,
+								ticketUrl,
+								furtherInfo,
+								furtherInfoContributorInitials,
+								isFree
+							}
 						}
 					}
 				}
 			}
 		}
-	}
-	`)
+		`)
+	} catch (e) {
+		console.log(e);
 
-	if (gigs){
-		gigs = gigs.venuesCollection.items[0].linkedFrom.eventsCollection.items.map((i) => {
+		return {
+			status: 404
+		};
+	}
+
+	if (data){
+		gigs = data.venuesCollection.items[0].linkedFrom.eventsCollection.items.map((i) => {
 			let { gigStartDate, ...rest } = i;
 			let d = new Date(gigStartDate);
 			return {
