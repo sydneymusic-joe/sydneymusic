93e2477eb17a61e392154e51ae232de062607ef1
Joe Hardy <joe@hardy.id.au>
2023-09-09 14:10:09 +0800

    Attempt scheduled-deploy fix

--- netlify/functions/scheduled-deploy/scheduled-deploy.js
+++ netlify/functions/scheduled-deploy/scheduled-deploy.js
@@ -1,11 +1,9 @@
-const { schedule } = require('@netlify/functions')
-const fetch = require('node-fetch');
+import { schedule } from '@netlify/functions';
+import fetch from 'node-fetch';
 
 const REBUILD_URL = process.env.BUILD_TRIGGER_URL;
 
-// To learn about scheduled functions and supported cron extensions,
-// see: https://ntl.fyi/sched-func
-module.exports.handler = schedule('10 14-15 * * *', async (event) => {
+export const handler = schedule('10 14-15 * * *', async (event) => {
   if (event.body) {
     const eventBody = JSON.parse(event.body)
     console.log(`Next function run at ${eventBody.next_run}.`)
