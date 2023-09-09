f67ee68e19b2f0006fce0d5da4424d4881510ec3
Joe Hardy <joe@hardy.id.au>
2023-09-09 20:00:48 +0800

    Revert "Attempt scheduled-deploy fix"
    This reverts commit 93e2477eb17a61e392154e51ae232de062607ef1.

--- netlify/functions/scheduled-deploy/scheduled-deploy.js
+++ netlify/functions/scheduled-deploy/scheduled-deploy.js
@@ -1,9 +1,11 @@
-import { schedule } from '@netlify/functions';
-import fetch from 'node-fetch';
+const { schedule } = require('@netlify/functions')
+const fetch = require('node-fetch');
 
 const REBUILD_URL = process.env.BUILD_TRIGGER_URL;
 
-export const handler = schedule('10 14-15 * * *', async (event) => {
+// To learn about scheduled functions and supported cron extensions,
+// see: https://ntl.fyi/sched-func
+module.exports.handler = schedule('10 14-15 * * *', async (event) => {
   if (event.body) {
     const eventBody = JSON.parse(event.body)
     console.log(`Next function run at ${eventBody.next_run}.`)
