c41df8dd07f6ceecd47eadd97b69f2cf03fe10a2
Joe Hardy <100741491+sydneymusic-joe@users.noreply.github.com>
2024-03-13 13:15:49 +1100

    bad merge problem! fix free gigs!

--- static/js/freegigs.js
+++ static/js/freegigs.js
@@ -1,5 +1,4 @@
 const freegigs = document.getElementById('toggle-freegigs');
-const freegigs = document.getElementById('toggle-freegigs');
 if (freegigs) {
 	freegigs.addEventListener('click', function () {
 		document.body.classList.toggle('freegigs');
