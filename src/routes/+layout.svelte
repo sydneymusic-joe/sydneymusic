8f9fce744b8cc10ec8002badf30b0a291e0f63d8
Joe Hardy <100741491+sydneymusic-joe@users.noreply.github.com>
2024-11-29 21:53:52 +1100

    give bottomsheet a break

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,6 @@
 	import '../app.css';
 	import Header from '$lib/components/header.svelte';
 	import Footer from '$lib/components/footer.svelte';
-	import BottomSheet from '$lib/components/bottom-sheet.svelte';
 	import { bottomSheetConfig } from '$lib/globals.mjs';
 </script>
 
@@ -10,7 +9,4 @@
 
 <Header />
 <slot />
-{#each bottomSheetConfig as sheet (sheet.key)}
-	<BottomSheet key={sheet.key} message={sheet.message} action={sheet.action} />
-{/each}
 <Footer />
