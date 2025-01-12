7d1eae4d9c1bccd6b14516eb300001f302a4b2aa
Joe Hardy <100741491+sydneymusic-joe@users.noreply.github.com>
2025-01-12 11:11:19 +1100

    Fixed link retrieval to paginate result sets

--- src/routes/links/+page.js
+++ src/routes/links/+page.js
@@ -2,19 +2,43 @@ import APId from '$lib/datocms/';
 import { groupBy } from '../../lib/globals.mjs';
 
 const getLinks = async () => {
-	const data = await APId(`{
-          allLinks(
-            orderBy: [category_ASC, title_ASC]
+	const pagesize = 100;
+	let iter = 0;
+	let ret = 100;
+
+	let data = [];
+	let query = '';
+	while (iter < 10) {
+		query += `
+		page${iter + 1}:allLinks(
+            orderBy: [category_ASC, title_ASC],
+			first: ${pagesize},
+			skip:${iter * pagesize}
           ) {
             title
             url
             description,
             category
           }
-        }`);
+		`;
+
+		iter++;
+	}
+
+	const page = await APId(`{
+		${query}
+	}`);
+
+	for (iter = 1; iter < 11; iter++) {
+		const p = page['page' + iter];
+		if (p.length == 0) break;
+
+		data = data.concat(p);
+	}
+	ret = data.length;
 
 	if (data) {
-		let byCategory = groupBy(data.allLinks, ({ category }) => category);
+		let byCategory = groupBy(data, ({ category }) => category);
 
 		return byCategory;
 	}
