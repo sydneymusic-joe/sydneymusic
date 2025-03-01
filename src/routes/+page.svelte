08536093cea261d3190d04496786e6afccb934c8
Joe Hardy <100741491+sydneymusic-joe@users.noreply.github.com>
2025-03-01 14:05:50 +1100

    3rd birthday

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -16,9 +16,10 @@
 	class="aspect-3/1 md:w-full lg:max-w-5xl mb-10 m-auto"
 />-->
 <div class="mb-10 mt-4 text-xs relative max-[400px]:text-red lg:max-w-5xl md:w-full m-auto">
-	<a href="https://sydneymusic.net/reads/2024-in-review-the-sydney-tracks-we-loved" class="hover:opacity-75 transition-opacity">
-		<img src="/2024tracks-promo.jpg" class="w-full" style="box-shadow : 0 10px 10px rgba(0,0,0,0.3)" alt="Likemind Society @ Oxford Art Factory, January 2024. Photo by Joe Hardy" />
+	<a href="https://sydneymusic.net/reads/sydneymusic-net-turns-3" class="hover:opacity-75 transition-opacity">
+		<img src="https://www.datocms-assets.com/137422/1740787466-3rd-og.jpg?w=2000" class="w-full" style="box-shadow : 0 10px 10px rgba(0,0,0,0.3)" alt="Likemind Society @ Oxford Art Factory, January 2024. Photo by Joe Hardy" />
 	
+		<!--
 		<div class="absolute bottom-5 left-5 text-white" style="text-shadow : 0px 1px 1px rgba(0,0,0,1)">
 			<div class="font-instrumentSans md:text-6xl text-4xl font-bold">2024 In Review:</div>
 			<div class="font-instrumentSerif uppercase text-2xl">The Sydney Tracks We Loved</div>
@@ -27,22 +28,18 @@
 		<div class="absolute top-5 right-5 text-black">
 			<div><img src="/smnlogo-black.png" class="w-32"></div>
 			<div class="font-instrumentSerif uppercase text-lg">It's A Gig Guide</div>
-		</div>
+		</div>-->
 	</a>
 
 	<div class="absolute bottom-5 right-5 gap-2 md:text-lg text-sm hidden xs:flex">
 		<PromotionButton
-			label="Full List &raquo;"
-			href="https://sydneymusic.net/reads/2024-in-review-the-sydney-tracks-we-loved"
+			label="Read Article &raquo;"
+			href="https://sydneymusic.net/reads/sydneymusic-net-turns-3"
 			variant="primary"
-		/>
-		<PromotionButton
-			label="Listen &raquo;"
-			href="https://www.patreon.com/posts/special-two-hour-120155379"
-			variant="secondary"
 		/>
 	</div>
 </div>
+
 
 <div class="max-w-5xl px-5 mx-auto space-y-20 pb-24">
 	<!-- First section -->
