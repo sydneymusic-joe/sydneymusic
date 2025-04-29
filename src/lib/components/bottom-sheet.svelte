09dd4602fc41ec471db05c113b1698082dd55992
Joe Hardy <100741491+sydneymusic-joe@users.noreply.github.com>
2025-04-29 22:05:42 +1000

    more cosmetics

--- src/lib/components/bottom-sheet.svelte
+++ src/lib/components/bottom-sheet.svelte
@@ -10,20 +10,20 @@
 	<div class="flex max-w-5xl m-auto gap-4 items-end">
 		<div class="space-y-5 basis-full md:basis-2/3">
 			<div class="space-y-3">
-				<div class="text-xl md:text-2xl">
+				<div class="text-lg font-semibold md:text-2xl">
 					<h1>SydneyMusic.net needs your support.</h1>
 					<h1>Donate now to help keep us running.</h1>
 				</div>
-				<p class="text-base">
+				<p class="text-sm md:text-base">
 					SydneyMusic.net is a not-for-profit initiative, built and maintained by a small team of passionate music lovers. We don't receive any government funding or run advertising - we run this site because we love this city and its incredible music scene.
 				</p>
-				<p class="text-base">
+				<p class="text-sm md:text-base">
 					Make a donation today to help us keep the site running, support the scene, and get more people to shows.
 				</p>
 			</div>
-			<div class="cta flex flex-col sm:flex-row gap-4 w-full">
+			<div class="cta flex flex-row gap-3 w-full">
 				<Button label="Make a donation" href="https://square.link/u/eS7bgm8M" target="_blank" variant="primary" />
-				<Button label="Learn more" href="/support" target="_blank" variant="secondary" />
+				<Button label="More info" href="/support" target="_blank" variant="secondary" />
 			</div>
 		</div>
 		<div class="hidden md:basis-1/3 md:block">
