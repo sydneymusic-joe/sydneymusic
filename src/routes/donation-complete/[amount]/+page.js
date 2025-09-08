a7a2900a4a3f1613e2800e80ed9eb1d3778dfc91
Joe Hardy <100741491+sydneymusic-joe@users.noreply.github.com>
2025-09-08 15:46:36 +0800

    add static routes for completed donations

--- src/routes/donation-complete/[amount]/+page.js
+++ src/routes/donation-complete/[amount]/+page.js
@@ -1,3 +1,4 @@
+/** @type {import('./$types').EntryGenerator} */
 import APId from '$lib/datocms/';
 
 const getReads = async () => {
@@ -14,6 +15,20 @@ const getReads = async () => {
 	}
 	return {};
 };
+
+export function entries() {
+	return [
+		{ amount: '2' },
+		{ amount: '5' },
+		{ amount: '10' },
+		{ amount: '15' },
+		{ amount: '50' },
+		{ amount: '100' },
+		{ amount: '200' },
+		{ amount: '500' },
+		{ amount: '1000' }
+	];
+}
 
 export async function load() {
 	let reads = await getReads();
