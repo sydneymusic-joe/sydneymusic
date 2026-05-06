598c21cd22f61241de609b60bd7c7ffee5174ddc
Joe Hardy <100741491+sydneymusic-joe@users.noreply.github.com>
2026-05-06 11:24:35 +1000

    test branch

--- src/lib/components/donation-form.svelte
+++ src/lib/components/donation-form.svelte
@@ -51,163 +51,88 @@
 	);
 </script>
 
-<div class={twMerge('w-full', shadow && 'shadow-lg')}>
-	<div
-		class={twMerge(
-			'p-5 flex flex-col gap-4',
-			theme === 'dark' && 'bg-[#282828] text-white',
-			theme === 'ruby' && 'bg-ruby text-white ',
-			theme === 'yellow' && 'bg-yellow-100 text-black',
-			theme === 'white' && 'bg-white text-black',
-			className
-		)}
-	>
-		<div class={twMerge('pr-20', size === 'sm' && 'pr-24')}>
-			<Heading
-				level={2}
-				variant={size}
-				class={twMerge('italic mb-0 text-pretty', size === 'sm' && 'text-lg')}>{headline}</Heading
-			>
-		</div>
-		<div class="relative">
-			<ProgressBar percentage={donorStats ? donorStats.totalFundedPercent : 0} {theme} {showPercentage} />
-			<img
-				src={theme === 'dark' ? '/images/canman-flagman.png' : '/images/canman-flagman.png'}
-				class="w-20 lg:w-28 absolute bottom-3 -right-6"
-				alt="Can Man holding a flag with Cliff the Glyph"
-			/>
-		</div>
+<div class="bg-merchbrown text-white text-center p-5 md:p-10 mx-[-20px] md:ml-0 md:mr-10">
+				<div class="guide-divider" style="margin-bottom : 0.25rem">
+					<div></div>
+					<div class="flex flex-row justify-center gap-x-3 mx-3">
+						<div><img src="/banksia.svg" class="h-12" alt="Pixelated Banksia icon" /></div>
+						<Heading class="text-ruby uppercase font-normal"><div class="stretch leading-[1.9] md:leading-[1.3]">Fundraiser FY27</div></Heading>
+						<div><img src="/padlock.svg" class="h-12" alt="Pixelated unlocked padlock icon" /></div>
+					</div>
+					<div></div>
+				</div>
+				<div class="space-y-3">
+					<div>SydneyMusic urgently needs your help.</div>
+					<div>This resource is 100% community-supported. To keep the gig guide online from July 1 we need to raise money towards our baseline budget for FY27 (we’ve got {donorStats.totalFundedPercent}% sorted already).</div>
+					<div class="font-semibold">We need to sign up <span class="text-ruby">{450-donorStats.recurringDonorCount}</span> more monthly supporters by June 30 to meet our goal for this form of support.</div>
+					<div>Your support is hugely appreciated. Thank you!</div>
+				</div>
+				<div class="flex flex-row justify-center gap-x-2 mt-5">
+				<img src="/horns.svg" class="h-12" alt="Pixelated horned hands icon" />
+				<h2 class="stretch uppercase mt-5 leading-[1.3]">Choose your fighter:</h2>
+				<img src="/canhand.svg" class="h-12" alt="Pixelated Can Man hand icon" />
+				</div>
+				<div class="flex flex-wrap flex-row justify-center gap-x-5 gap-y-3 my-5">
+					<label class="relative flex items-center cursor-pointer">
+						<input type="radio" name="donoramount" data-buttonhref="https://square.link/u/L0GTRapn" class="sr-only peer" />
+						<div class="w-6 h-6 bg-transparent border-2 border-ruby rounded-full peer-checked:bg-ruby peer-checked:border-ruby"></div>
+						<span class="ml-2 text-white">$2</span>
+					</label>
+					<label class="relative flex items-center cursor-pointer">
+						<input type="radio" name="donoramount" data-buttonhref="https://square.link/u/Gw8URGj2" class="sr-only peer" />
+						<div class="w-6 h-6 bg-transparent border-2 border-ruby rounded-full peer-checked:bg-ruby peer-checked:border-ruby"></div>
+						<span class="ml-2 text-white">$5</span>
+					</label>
+					<label class="relative flex items-center cursor-pointer">
+						<input type="radio" name="donoramount" data-buttonhref="https://square.link/u/hbwswm65" checked="" class="sr-only peer" />
+						<div class="w-6 h-6 bg-transparent border-2 border-ruby rounded-full peer-checked:bg-ruby peer-checked:border-ruby"></div>
+						<span class="ml-2 text-white">$10</span>
+					</label>
+					<label class="relative flex items-center cursor-pointer">
+						<input type="radio" name="donoramount" data-buttonhref="https://square.link/u/otfmCL39" class="sr-only peer" />
+						<div class="w-6 h-6 bg-transparent border-2 border-ruby rounded-full peer-checked:bg-ruby peer-checked:border-ruby"></div>
+						<span class="ml-2 text-white">$25</span>
+					</label>
+					<label class="relative flex items-center cursor-pointer">
+						<input type="radio" name="donoramount" data-buttonhref="https://square.link/u/moL86XF5" class="sr-only peer" />
+						<div class="w-6 h-6 bg-transparent border-2 border-ruby rounded-full peer-checked:bg-ruby peer-checked:border-ruby"></div>
+						<span class="ml-2 text-white">$50</span>
+					</label>
+					<label class="relative flex items-center cursor-pointer">
+						<input type="radio" name="donoramount" data-buttonhref="https://square.link/u/Dicl3TrR" class="sr-only peer" />
+						<div class="w-6 h-6 bg-transparent border-2 border-ruby rounded-full peer-checked:bg-ruby peer-checked:border-ruby"></div>
+						<span class="ml-2 text-white">$200</span>
+					</label>
+					<label class="relative flex items-center cursor-pointer">
+						<input type="radio" name="donoramount" data-buttonhref="https://square.link/u/PbEmze4h" data-buttonlabel="Make a one-off donation" class="sr-only peer	" />
+						<div class="w-6 h-6 bg-transparent border-2 border-ruby rounded-full peer-checked:bg-ruby peer-checked:border-ruby"></div>
+						<span class="ml-2 text-white">I’d like to make a one-off donation</span>
+					</label>
+					<label class="relative flex items-center cursor-pointer">
+						<input type="radio" name="donoramount" class="sr-only peer" data-buttonhref="https://store.sydneymusic.net" data-buttonlabel="Shop for merch" />
+						<div class="w-6 h-6 bg-transparent border-2 border-ruby rounded-full peer-checked:bg-ruby peer-checked:border-ruby"></div>
+						<span class="ml-2 text-white">I’d like to buy a t-shirt or stickers!</span>
+					</label>
+				</div>
 
-		{#if appealContent}
-			<Paragraph variant={size === "lg" ? "sm" : "xs"} class="mb-0 leading-tight">
-				{appealContent}
-			</Paragraph>
-		{/if}
-		<div>
-			<Paragraph variant={size === "lg" ? "sm" : "xs"} class="mb-2 leading-tight lg:leading-tight">
-				<strong class="font-bold">{callToAction}</strong>
-			</Paragraph>
-			<Paragraph variant="xs" class="mb-0">Donate monthly:</Paragraph>
-		</div>
-		<div>
-			<div
-				class={twMerge(
-					'flex flex-row flex-wrap w-full text-md lg:text-xl gap-1',
-					size === 'lg' && 'gap-2'
-				)}
-			>
-				<Button
-					label="2"
-					prefix="$"
-					href="https://square.link/u/L0GTRapn"
-					target="_blank"
-					variant="outline"
-					pointer=""
-					{size}
-					class={`${buttonClass} plausible-event-amount=2`}
-					eventName="Donation+Amount"
-				/>
-				<Button
-					label="5"
-					prefix="$"
-					href="https://square.link/u/Gw8URGj2"
-					target="_blank"
-					variant="outline"
-					pointer=""
-					{size}
-					class={`${buttonClass} plausible-event-amount=5`}
-					eventName="Donation+Amount"
-				/>
-				<Button
-					label="15"
-					prefix="$"
-					href="https://square.link/u/JYzf9LTF"
-					target="_blank"
-					variant="outline"
-					pointer=""
-					{size}
-					class={`${buttonClass} plausible-event-amount=15`}
-					eventName="Donation+Amount"
-				/>
-				<Button
-					label="50"
-					prefix="$"
-					href="https://square.link/u/moL86XF5"
-					target="_blank"
-					variant="outline"
-					pointer=""
-					{size}
-					class={`${buttonClass} plausible-event-amount=50`}
-					eventName="Donation+Amount"
-				/>
-				<Button
-					label="200"
-					prefix="$"
-					href="https://square.link/u/Dicl3TrR"
-					target="_blank"
-					variant="outline"
-					pointer=""
-					{size}
-					class={`${buttonClass} plausible-event-amount=200`}
-					eventName="Donation+Amount"
-				/>
-			</div>
-		</div>
-		<div>
-			<Paragraph variant="xs" class="mb-1 font-light">
-				Donate a <a
-					href="https://square.link/u/eS7bgm8M"
-					target="_blank"
-					class="underline font-light plausible-event-name=Donation+Other"
-				>
-					different amount</a
-				>
-				or
-				<a href="https://square.link/u/eS7bgm8M" target="_blank" class="underline font-light plausible-event-name=Donation+OnceOff"
-					>donate once-off</a
-				>.
-			</Paragraph>
-			{#if showFineprint}
-				<p class="font-light text-xs md:text-md">
-					All donations of $2 and above are tax deductible. {#if size === 'lg'}
-						SydneyMusic.net is a not-for-profit initiative, built and maintained by a small team of
-						music lovers. We don't receive any government funding. We don't run advertising. <a
-							href="/support"
-							target="_blank"
-							class="underline plausible-event-name=Donation+LearnMore">Learn more &raquo;</a
-						>{/if}
-				</p>
-			{/if}
-		</div>
-	</div>
-	{#if showSponsors}
-		<div class="border-t border-gray-200 bg-white p-5" class:shadow-lg={shadow}>
-			<Paragraph variant="xs" class="mb-0">Thanks to {mds ? mds.recurringDonorCount + mds.oneOffDonors : 290} individuals and these legends:</Paragraph>
+				<div class="space-y-2">
+				<Button label="Become a monthly supporter" variant="primary" id="support-button" href="https://square.link/u/hbwswm65" target="_blank" />
+				<Button label="Learn more" href="/support" target="_blank" />
+				</div>
 
-			<div class="flex flex-wrap flex-row text-xs gap-4 my-2 items-center justify-start">
-				<img src="/sponsors-rode.svg" alt="Røde" class="h-4" />
-				<img src="/logos/heaps-normal.png" class="h-8" alt="Heaps Normal" />
-				<div class="uppercase opacity-40 font-bold italic">
-					<a href="/support/commercial">+ your name here?</a>
+				<script>
+					var donorItems = document.querySelectorAll('input[name=donoramount]');
+					donorItems.forEach(el => {
+						el.addEventListener('click', function (evt) {
+							document.getElementById('support-button').href = evt.srcElement.attributes['data-buttonhref'].value;
+							document.getElementById('support-button').innerHTML = (evt.srcElement.attributes['data-buttonlabel'] ? evt.srcElement.attributes['data-buttonlabel'].value : "Become a monthly supporter") + " &raquo;";
+						});
+					});
+				</script>
+
+				<div class="mt-2 text-xs">All donations over $2 are tax deductible. SydneyMusic is a registered not-for-profit organisation and is recognised as a Deductible Gift Recipient by the ATO</div>
+				<div class="mt-2 text-xs">The average donation from our {donorStats.recurringDonorCount} supporters is $10</div>
+				<div class="mt-2 text-xs">
+					<a href="/support/commercial" class="underline">Commercial enquiries</a> • <a href="/api-license" class="underline">API licensing</a> • <a href="mailto:contact@sydneymusic.net" class="underline">General enquiries</a>
 				</div>
 			</div>
-			<div class="space-y-2">
-				<div class="flex flex-row gap-x-2">
-					<Button
-						label="Become a Commercial Sponsor"
-						variant="outline"
-						class="bg-white"
-						pointer=""
-						href="/support/commercial"
-					/>
-				</div>
-				<div class="text-xs">
-					<a
-						href="/support/wall-of-legends"
-						class="text-black underline hover:text-ruby focus:text-ruby">See the Wall of Legends</a
-					>
-				</div>
-			</div>
-		</div>
-	{/if}
-</div>
---
+++ src/routes/support/tote/+page.svelte
@@ -0,0 +1,35 @@
+<script>
+	import Button from '$lib/components/button.svelte';
+	import SeoSocial from '$lib/components/seo-social.svelte';
+</script>
+
+<SeoSocial title="SydneyMusic FY27 Fundraiser" />
+
+<div class="bg-merchbrown">
+	<div class="flex flex-col md:flex-row">
+		<div class="min-h-[300px] flex p-12">
+			<video controls class="mx-auto aspect-9/16 h-full max-h-[500px]" poster="/video/PartyDozen-thumb.png">
+				<source src="/video/PartyDozen.webm" type="video/webm" />
+			</video>
+		</div>
+		<div class="flex-1 text-white p-12 flex items-center">
+			<div class="space-y-5">
+				<h1 class="text-2xl stretch">The ultimate Sydney music tote bag!</h1>
+				<p>The SydneyMusic gig guide is a labour of love that takes 60 hours per week to maintain. To keep the guide in good health, we need to raise our FY27 operational budget.</p>
+				<p>To help raise funds for our survival, we have the <strong>last-ever Can Man tote bag from our first merch drop in 2022</strong>, and we’re filling it with an incredible one-of-a-kind collection of merch from Sydney artists &mdash; and it could be yours to win!</p>
+				<p>Thank you to Party Dozen for kicking things off!</p>
+				<h2 class="text-xl stretch">What’s in the bag?</h2>
+				<p>We’ll keep updating this list as we go. So far we have:</p>
+				<ul>
+					<li><strong>Party Dozen — The Invisible Man</strong><br /><span class="text-sm">Vinyl LP • Original pressing • Last copy of 275</span></li>
+				</ul>
+				<p>Want to contribute? <a href="mailto:contact@sydneymusic.net" class="underline">Get in touch</a> for more information.</p>
+				<Button variant="primary" label="Enquire about donating some merch" href="mailto:contact@sydneymusic.net" />
+				<h2 class="text-xl stretch">Support SydneyMusic!</h2>
+				<p><strong>Your gig guide needs your help!</strong></p>
+				<Button variant="primary" label="Make a donation" />
+				<Button variant="secondary" label="Buy some merch" />
+			</div>
+		</div>
+	</div>
+</div>
