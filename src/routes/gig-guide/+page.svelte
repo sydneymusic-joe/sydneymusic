5380aa686334f3b019c0e5227d9116189570b28b
Joe Hardy <100741491+sydneymusic-joe@users.noreply.github.com>
2026-05-06 16:27:41 +1000

    improve big flash of image (needs additional work)

--- src/routes/gig-guide/+page.svelte
+++ src/routes/gig-guide/+page.svelte
@@ -61,7 +61,7 @@
 			</div>
 			<div class="font-semibold text-center md:mr-10">There are {new Intl.NumberFormat("en-AU").format(data.gigs.totalCount)} gigs to be explored below.</div>
 			<div class="flex flex-col items-center md:mr-10 my-5 gap-y-5">
-				<div class="w-[75%]"><a href="/support/tote"><img src="/pdtote.png" class="w-full" style="box-shadow : 10px 10px 0px rgba(0,0,0,0.5); border : solid 3px black" /></a></div>
+				<div class="w-[75%]"><a href="/support/tote"><img src="/pdtote.png" class="w-full max-w-[500px]" style="box-shadow : 10px 10px 0px rgba(0,0,0,0.5); border : solid 3px black" /></a></div>
 				<div class="text-center text-sm"><strong>WATCH:</strong> <a href="/support/tote" class="underline">Party Dozen launch new fundraiser!</a></div>
 			</div>
 			<div class="flex flex-col items-center md:mr-10 my-10">
