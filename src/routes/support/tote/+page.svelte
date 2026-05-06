9215489318a0cc8afc49a76daaf8ee55d714e332
Joe Hardy <100741491+sydneymusic-joe@users.noreply.github.com>
2026-05-06 15:23:08 +1000

    updates

--- src/routes/support/tote/+page.svelte
+++ src/routes/support/tote/+page.svelte
@@ -16,23 +16,29 @@
 			<Button variant="primary" label="E-mail us to contribute!" href="mailto:contact@sydneymusic.net" />
 		</div>
 		<div class="flex-1 text-white p-12 flex items-center">
-			<div class="space-y-5">
-				<Heading class="text-2xl stretch">The ultimate Sydney music tote bag!</Heading>
-				<p>We’ve got the <strong>last-ever Can Man tote bag from our first merch drop in 2022</strong>.</p>
-				<p>In the next few weeks we’re going to fill it with donated items from Sydney artists — creating an incredible one-of-a-kind collection for one lucky winner.</p>
-				<p>The tote will be raffled at a very special event soon — stay tuned for more info!</p>
-				<p>Thank you to <strong>Party Dozen</strong> for kicking things off!</p>
-				<Heading class="text-xl stretch">What’s in the bag?</Heading>
-				<p>We’ll keep updating this list as we go. So far we have:</p>
-				<ul>
-					<li><strong>Party Dozen — The Invisible Man</strong><br /><span class="text-sm">Vinyl LP • Original pressing • Last copy of 275</span></li>
-				</ul>
-				<p>Want to contribute? <a href="mailto:contact@sydneymusic.net" class="underline">Get in touch</a> for more information.</p>
-				<Heading class="text-xl stretch">Support SydneyMusic!</Heading>
-				<p><strong>Sydney’s most comprehensive gig guide needs your help!</strong></p>
-				<p>We need to raise $30,000 to help cover our operating expenses for the first half of FY27.</p>
-				<Button variant="primary" label="Make a donation" />
-				<Button variant="secondary" label="Buy some merch" />
+			<div class="space-y-8">
+				<Heading class="text-2xl font-normal uppercase stretch leading-[1.0]">The ultimate Sydney music tote bag!</Heading>
+				<div class="space-y-3">
+					<p>We’ve got the <strong>last-ever Can Man tote bag from our first merch drop in 2022</strong>.</p>
+					<p>In the next few weeks we’re going to fill it with donated items from Sydney artists — creating an incredible one-of-a-kind collection for one lucky winner.</p>
+					<p>The tote will be raffled at a very special event soon — stay tuned for more info!</p>
+					<p>Thank you to <strong>Party Dozen</strong> for kicking things off!</p>
+				</div>
+				<Heading variant="md" class="uppercase font-normal stretch">What’s in the bag?</Heading>
+				<div class="space-y-3">
+					<p>We’ll keep updating this list as we go. So far we have:</p>
+					<ul>
+						<li><strong>Party Dozen — The Invisible Man</strong><br /><span class="text-sm">Vinyl LP • Original pressing • Last copy of 275</span></li>
+					</ul>
+					<p>Want to contribute? <a href="mailto:contact@sydneymusic.net" class="underline">Get in touch</a> for more information.</p>
+				</div>
+				<Heading variant="md" class="uppercase font-normal stretch">Support SydneyMusic!</Heading>
+				<div class="space-y-3">
+					<p><strong>Sydney’s most comprehensive gig guide needs your help!</strong></p>
+					<p>We need to raise $30,000 to help cover our operating expenses for the first half of FY27.</p>
+					<Button variant="primary" label="Make a donation" />
+					<Button variant="secondary" label="Buy some merch" />
+				</div>
 			</div>
 		</div>
 	</div>
