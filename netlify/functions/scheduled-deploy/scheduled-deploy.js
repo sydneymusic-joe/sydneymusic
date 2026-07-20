34a2db1a22497a691ea2806ca83ae756304e6508
Joe Hardy <100741491+sydneymusic-joe@users.noreply.github.com>
2026-07-20 10:36:49 +1000

    Netlify suddenly breaking on scheduled deploy. don't know why this is suddenly an issue, but it seems to be complaining about CJS v ECMA ... so converting to ECMA

--- netlify/functions/scheduled-deploy/scheduled-deploy.js
+++ netlify/functions/scheduled-deploy/scheduled-deploy.js
@@ -1,14 +1,22 @@
-const { schedule } = require('@netlify/functions');
-const fetch = require('node-fetch');
+import { schedule } from '@netlify/functions';
+import fetch from 'node-fetch';
 
 const REBUILD_URL = process.env.BUILD_TRIGGER_URL;
 
 // To learn about scheduled functions and supported cron extensions,
 // see: https://ntl.fyi/sched-func
-module.exports.handler = schedule('10 14-15 * * *', async (event) => {
+export const handler = schedule('10 14-15 * * *', async (event) => {
 	if (event.body) {
 		const eventBody = JSON.parse(event.body);
 		console.log(`Next function run at ${eventBody.next_run}.`);
+	}
+
+	if (!REBUILD_URL) {
+		console.error('BUILD_TRIGGER_URL is not configured.');
+		return {
+			statusCode: 500,
+			body: 'BUILD_TRIGGER_URL is not configured.'
+		};
 	}
 
 	await fetch(REBUILD_URL, { method: 'POST' });
